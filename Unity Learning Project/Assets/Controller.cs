b4d01b762bbcada998adb358ea8844534b76c695
Qache <gortatm@gmail.com>
2020-04-25 17:01:37 +0200

    Added basic movement controller.

---
+++ Unity Learning Project/Assets/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	[SerializeField] private Rigidbody2D rb;
+	[SerializeField] private float speed;
+	[SerializeField] private Vector2 moveVelocity;
+    void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		moveVelocity = moveInput.normalized * speed;
+	}
+
+	private void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+	}
+}
