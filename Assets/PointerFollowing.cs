835dc49be2483ed9c99bf56a064d3a7ff329f429
Qache <gortatm@gmail.com>
2020-05-18 17:21:56 +0200

    Fixed player rotation and all coordinates. Changed plane to be smaller

---
+++ Assets/PointerFollowing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerFollowing : MonoBehaviour
+{
+	[SerializeField] private Vector3 pointerPosition;
+    void Start()
+    {
+        
+    }
+	private void Update()
+	{
+		pointerPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		pointerPosition.Set(pointerPosition.x, pointerPosition.y, 0);
+		transform.LookAt(pointerPosition);
+	}
+}
