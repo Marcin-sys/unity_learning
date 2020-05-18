51abc63f95f124c98c210e9b8b17259bed09cb94
Marcin M <mircin27@gmail.com>
2020-05-18 18:03:22 +0200

    Added shooting option
    Added bullet. Added GunTip for location of bullet spawn. Added shooting option.

--- Assets/PointerFollowing.cs
+++ Assets/PointerFollowing.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class PointerFollowing : MonoBehaviour
 {
 	[SerializeField] private Vector3 pointerPosition;
-    void Start()
+	[SerializeField] private Transform gunTip;
+	[SerializeField] private GameObject bullet;
+
+	void Start()
     {
-        
+
     }
 	private void Update()
 	{
 		pointerPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		pointerPosition.Set(pointerPosition.x, pointerPosition.y, 0);
 		transform.LookAt(pointerPosition);
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			FireBullet();
+		}
+	}
+	private void FireBullet()
+	{
+		GameObject firedBullet = Instantiate(bullet, gunTip.position, gunTip.rotation);
+		firedBullet.GetComponent<Rigidbody>().velocity = gunTip.up * 10f;
 	}
 }
